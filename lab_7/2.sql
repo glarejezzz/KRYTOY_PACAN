79d483cceac1f7f6fb860fdde075126cfbaf5256
mediocre <w1rs01769@gmail.com>
2026-05-13 15:43:57 +0300

    Add files via upload

---
+++ lab_7/2.sql
@@ -0,0 +1,36 @@
+﻿USE Tour
+GO
+
+ALTER TABLE marshrut ADD LastModifiedDate DATETIME DEFAULT GETDATE();
+GO
+
+CREATE TRIGGER trg_route_update
+ON marshrut
+AFTER UPDATE
+AS
+BEGIN
+	UPDATE marshrut
+	SET LastModifiedDate = GETDATE()
+	WHERE id_route in (SELECT id_route FROM inserted)
+	PRINT 'Дата изменения обновлена';
+
+END;
+GO;
+
+UPDATE marshrut SET duration_days = 15 WHERE id_route = 1;
+GO
+
+SELECT id_route, name_route, duration_days, LastModifiedDate 
+FROM marshrut 
+WHERE id_route = 1;
+GO
+
+
+
+--    ▄▄▄▄▄   █  █▀ ▄█ ▄█ ▄███▄     ▄▄▄▄▄   ▄███▄   ██  
+--   █     ▀▄ █▄█   ██ ██ █▀   ▀   █     ▀▄ █▀   ▀  █ █ 
+-- ▄  ▀▀▀▀▄   █▀▄   ██ ██ ██▄▄   ▄  ▀▀▀▀▄   ██▄▄    █▄▄█
+--  ▀▄▄▄▄▀    █  █  ▐█ ▐█ █▄   ▄▀ ▀▄▄▄▄▀    █▄   ▄▀ █  █
+--              █    ▐  ▐ ▀███▀             ▀███▀      █
+--             ▀                                      █ 
+--            
---
+++ lab_7/4.sql
@@ -0,0 +1,42 @@
+﻿
+USE Tour
+GO
+
+ALTER TABLE sale ADD VisaRequired NVARCHAR(20) DEFAULT NULL;
+GO
+
+CREATE TRIGGER trg_sale_visa
+ON sale
+AFTER INSERT
+AS
+BEGIN
+    UPDATE s
+    SET VisaRequired = IIF(c.cost_visa = 0, 'Нет', 'Да')
+    FROM sale s
+    INNER JOIN inserted i ON s.id_sale = i.id_sale
+    INNER JOIN marshrut m ON i.id_route = m.id_route
+    INNER JOIN country c ON m.id_country = c.id_country;
+END
+GO
+
+INSERT INTO sale (id_client, id_route, number_contract, full_name_client, name_route, destination_country, ticket_price, quantity_ticket, date_sale, sales_manager) 
+VALUES (1, 4, 'TEST-VISA-001', 'Кузнецов Сергей', 'Россия', 'Москва', 2000, 5, GETDATE(), 'Менеджер'),
+(2, 6, 'TEST-VISA-003', 'Кузнецов Сергей', 'Дубай', 'ОАЭ', 1500, 1, GETDATE(), 'Менеджер');
+
+
+SELECT id_client, id_route, name_route, destination_country, VisaRequired 
+FROM sale 
+WHERE number_contract = 'TEST-VISA-003';
+
+SELECT id_client, id_route, name_route, destination_country, VisaRequired 
+FROM sale 
+WHERE number_contract = 'TEST-VISA-001';
+
+
+--    ▄▄▄▄▄   █  █▀ ▄█ ▄█ ▄███▄     ▄▄▄▄▄   ▄███▄   ██  
+--   █     ▀▄ █▄█   ██ ██ █▀   ▀   █     ▀▄ █▀   ▀  █ █ 
+-- ▄  ▀▀▀▀▄   █▀▄   ██ ██ ██▄▄   ▄  ▀▀▀▀▄   ██▄▄    █▄▄█
+--  ▀▄▄▄▄▀    █  █  ▐█ ▐█ █▄   ▄▀ ▀▄▄▄▄▀    █▄   ▄▀ █  █
+--              █    ▐  ▐ ▀███▀             ▀███▀      █
+--             ▀                                      █ 
+--            
